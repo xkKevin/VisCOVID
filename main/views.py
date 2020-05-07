bc1a62730b54f0eb57ce54c496199e2d3c96f3f1
Kai Xiong <xkkevinbear@gmail.com>
2020-05-07 23:57:43 +0800

    add dataview

--- main/views.py
+++ main/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from handleData.prepare import prepare
 from handleData.analyze import analyze
+import base64, json
 # Create your views here.
 wxb_name = "./handleData/data/wang.xlsx"
 world_name = "./handleData/data/owd.csv"
 export_path = "./main/static/export"
+report_path = "./main/static/report"
 def index(request):
     return render(request, "index.html")
 
@@ -31,8 +33,25 @@ def report(request):
                 analyze()
 
         except Exception as e:
-            print(e)
-            return JsonResponse({"error": "Please upload those two files correctly!"})
+            return JsonResponse({"error": "Error!\n"+repr(e)})
         return render(request, "report.html")
     else:
         return render(request, "report.html")
+
+
+def saveImage(request):
+    if request.method == "POST":
+        try:
+            # baseimg = json.loads(request.POST.get("baseimg"))
+            baseimg = request.POST.get("baseimg")
+            print(baseimg)
+            with open("%s/%s.png" % (report_path, '2'), 'wb') as f:
+                f.write(base64.b64decode(baseimg[21:]))
+            # for key, value in baseimg.items():
+            #     with open("%s/%s.png" % (report_path, key),'wb') as f:
+            #         f.write(base64.b64decode(value[21:]))
+        except Exception as e:
+            return JsonResponse({"error": "Error!\n"+repr(e)})
+        return JsonResponse({"result": True})
+
+    return JsonResponse({"result": "404"})
