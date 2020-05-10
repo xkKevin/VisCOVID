c41b44dd0bcd6184e9a84d2c6425c4d4251bec8c
Kai Xiong <xkkevinbear@gmail.com>
2020-05-10 15:26:20 +0800

    v1.0

--- VisCOVID/urls.py
+++ VisCOVID/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('report/', views.report, name="report"),
     path('saveImage/', views.saveImage, name="saveImage"),
+    path('export/<file_name>/', views.csvFile, name='csvFile')
 ]
